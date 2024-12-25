4ede21ef7c70d9879d53372cb40546aad21bcddc
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-12-25 12:57:01 +0100

    Initial Commit

---
+++ UglyChristmas/UglyChristmasApp.swift
@@ -0,0 +1,17 @@
+//
+//  UglyChristmasApp.swift
+//  UglyChristmas
+//
+//  Created by MacBook AIR on 25/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct UglyChristmasApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
